06465be8f304c3cf599515bde50a26d0df1ba859
gustavomarim <gustavo.dantas.marim@outlook.com>
2022-04-16 00:46:09 -0300

    Consumindo API

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 const REQUEST_PATH = "./data/data.json";
 
+const timeFrames = document.querySelectorAll(".profile-time-frames li");
+
+function getTimeFrameLi(event) {
+  const { id } = event.target;
+  // console.log("id", id);
+  return id;
+}
+
+timeFrames.forEach((timeFrame) => {
+  timeFrame.addEventListener("click", getTimeFrameLi);
+});
+
 function getData() {
-  fetch(REQUEST_PATH).then((response) => response.json());
+  fetch(REQUEST_PATH)
+    .then((response) => response.json())
+    .then((response) => {
+      const data = response;
+      return data;
+    });
 }
 
 getData();
